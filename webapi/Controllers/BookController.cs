4fa6298ea325f7c8e87767caebed0ae306b3baa1
andres655 <andres6555@gmail.com>
2022-01-13 13:38:19 -0500

    HttpStatusCode

--- webapi/Controllers/BookController.cs
+++ webapi/Controllers/BookController.cs
@@ -8,31 +8,32 @@ namespace webapi.Controllers
     public class BookController : ControllerBase
     {
         [HttpGet]
-        public List<Book> Get()
+        public IActionResult Get()
         {
-            return BookData.Listar();
+
+            return Ok(BookData.Listar());
         }
 
         [HttpGet("{id}")]
         public Book Get(int ID)
         {
-            return BookData.Obtener(ID);
+            return  BookData.Obtener(ID);
         }
 
         [HttpPost]
-        public bool Post([FromBody]Book book)
+        public IActionResult Post([FromBody]Book book)
         {
-           return BookData.Agregar_Book(book);
+           return Ok(BookData.Agregar_Book(book));
         }
         [HttpPut]
-        public bool Put([FromBody] Book book)
+        public IActionResult Put([FromBody] Book book)
         {
-            return BookData.Modificar_Book(book);
+            return Ok(BookData.Modificar_Book(book));
         }
         [HttpDelete]
-        public  bool Delete (int id)
+        public  IActionResult Delete (int id)
         {
-           return BookData.Eliminar(id);
+           return Ok(BookData.Eliminar(id));
         }
     }
 }
