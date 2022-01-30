b72d10a24f1545409085986a1d4c6f7beaccc09c
andres655 <andres6555@gmail.com>
2022-01-30 11:27:56 -0500

    Prueba de integracion y test unitarios post y put

---
+++ TestWebApi/TestIntegracionWebApi.cs
@@ -0,0 +1,57 @@
+﻿
+using webapi.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using webapi;
+
+namespace TestWebApi
+{
+   public class TestIntegracionWebApi
+    {
+        [Fact]
+        public void TestInsertBook_Then_ModifyIt()
+        {
+        
+            BookController bookController = new BookController();
+            int  ID = 5;
+            
+                Book defaultPRofile = BuildBookProfile(ID);
+            //llama al servicio post
+            bookController.Post(defaultPRofile);
+            //llama prueba el servicio get
+            Book userStep1 = (Book)bookController.Get(ID);
+            
+                Assert.AreEqual(defaultPRofile.Id, userStep1.Id);
+                Assert.AreEqual(defaultPRofile.Titulo, userStep1.Titulo);
+                Assert.AreEqual(defaultPRofile.Autor, userStep1.Autor);
+
+            //llama al servicio put
+            bookController.Put(defaultPRofile);
+
+            Book userStep2 = (Book)bookController.Get(ID);
+                Assert.AreEqual(defaultPRofile.Id, userStep2.Id);
+                Assert.AreEqual(defaultPRofile.Titulo, userStep2.Titulo);
+                Assert.AreEqual(defaultPRofile.Autor, userStep2.Autor);
+
+           
+        }
+
+        private Book BuildBookProfile(int ID)
+        {
+            return new Book()
+            {
+               Id=5,    
+               Titulo="La Vuelta al Mundo",
+               Descripcion="En 80 dias",
+               Autor="Julio Verne"
+               
+            };
+        }
+    }
+}
--- TestWebApi/UnitTest1.cs
+++ TestWebApi/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using webapi;
 using webapi.Controllers;
 
 namespace TestWebApi
@@ -9,9 +10,9 @@ namespace TestWebApi
         [TestMethod]
         public void TestDelete()
         {
-            BookController bookController= new BookController();
+            BookController bookController = new BookController();
 
-            bool result =bookController.Delete(3);
+            bool result = bookController.Delete(0);
             Assert.IsTrue(result);
         }
 
@@ -20,8 +21,50 @@ namespace TestWebApi
         {
             BookController bookController = new BookController();
 
-            webapi.Book result = bookController.Get(1);
-            Assert.AreEqual(1, result.Id);
+            webapi.Book result = (webapi.Book)bookController.Get(0);
+            Assert.AreEqual(0, result.Id);
+        }
+
+        [TestMethod]
+        public void TestPost()
+        {
+            BookController bookController = new BookController();
+            int ID = 5;
+            Book defaultPRofile = BuildBookProfile(ID);
+            // var BookAppService = serviceProvider.GetRequiredService<BookController>();
+            //BookAppService.Post(defaultPRofile);
+            bookController.Post(defaultPRofile);
+            Book userStep1 = (Book)bookController.Get(ID);
+
+            Assert.AreEqual(defaultPRofile.Id, userStep1.Id);
+            Assert.AreEqual(defaultPRofile.Titulo, userStep1.Titulo);
+            Assert.AreEqual(defaultPRofile.Autor, userStep1.Autor);
+        }
+
+        [TestMethod]
+        public void TestPut()
+        {
+            BookController bookController = new BookController();
+            int ID = 5;
+            Book defaultPRofile = BuildBookProfile(ID);
+            bookController.Put(defaultPRofile);
+
+            Book userStep2 = (Book)bookController.Get(ID);
+            Assert.AreEqual(defaultPRofile.Id, userStep2.Id);
+            Assert.AreEqual(defaultPRofile.Titulo, userStep2.Titulo);
+            Assert.AreEqual(defaultPRofile.Autor, userStep2.Autor);
+        }
+
+        private Book BuildBookProfile(int ID)
+        {
+            return new Book()
+            {
+                Id = 5,
+                Titulo = "La Vuelta al Mundo",
+                Descripcion = "En 80 dias",
+                Autor = "Julio Verne"
+
+            };
         }
     }
 }
